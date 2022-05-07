fba6b8b2672ef56d0c7e39fd50bc68ff013d1e8e
Phuong Vy <phuongvy.maicogroup@gmail.com>
2022-05-07 18:55:04 +0700

    theem api cham diem

--- src/controllers/quizz.js
+++ src/controllers/quizz.js
@@ -2,56 +2,57 @@ import Quizz from "../models/quizz";
 import express from "express";
 
 export const getAllByCourseId = async (req, res) => {
-      try {
-            const { courseId } = req.params;
-            const quizzs = await Quizz.find({ course: courseId })
-            if (!quizzs) return res.status(404).json({ message: "not-found" })
-            return res.status(200).json({ quizzIds: quizzs.map(item => item._id.toString()) })
-      } catch (error) {
-            res.status(500).json({ error })
-            throw new Error(error)
-      }
-}
-
+  try {
+    const { courseId } = req.params;
+    const quizzs = await Quizz.find({ course: courseId });
+    if (!quizzs) return res.status(404).json({ message: "not-found" });
+    return res
+      .status(200)
+      .json({ quizzIds: quizzs.map((item) => item._id.toString()) });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+};
 
 export const getById = async (req, res) => {
-      try {
-            const quizz = await Quizz.findById(req.params.id)
-            if (!quizz)
-                  return res.status(404).json({ message: "quizz-not-found" })
-            const { answers, ...ignoredAnwser } = quizz._doc
-            return res.status(200).json({ ignoredAnwser })
-      } catch (error) {
-            res.status(500).json({ error })
-      }
-}
+  try {
+    const quizz = await Quizz.findById(req.params.id);
+    if (!quizz) return res.status(404).json({ message: "quizz-not-found" });
+    const { answers, ...ignoredAnwser } = quizz._doc;
+    return res.status(200).json({ ignoredAnwser });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+};
 
 export const createQuizz = async (req, res) => {
-      try {
-            const newQuizz = new Quizz(req.body)
+  try {
+    const newQuizz = new Quizz(req.body);
 
-            const quizz = await newQuizz.save()
+    const quizz = await newQuizz.save();
 
-            return res.status(200).json({ quizz })
-      } catch (error) {
-            res.status(500).json({ error })
-      }
-}
+    return res.status(200).json({ quizz });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+};
 
 export const markById = async (req, res) => {
-      try {
-            const { quizzId, answers } = req.body
-            const quiz = await Quizz.findById(quizzId)
-            const { result } = await quiz._doc
+  try {
+    const { id } = req.params;
+    const { anwsers } = req.body;
+    const quiz = await Quizz.findById(id);
+    const { result } = await quiz._doc;
 
-            const point = result.reduce((prev, curr, index) => {
-                  if (curr === answers[index]) return prev + 1
-                  return prev
-            }, 0)
-            console.log(point)
+    const point = await result.reduce(
+      (prev, curr, index) => (curr === anwsers[index] ? prev + 1 : prev),
+      0
+    );
 
-            res.status(200).json({ point })
-      } catch (error) {
-            res.status(500).json({ error })
-      }
-}
+    res
+      .status(200)
+      .json({ point: +((point / anwsers.length) * 10).toFixed(2) });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+};
--- src/routes/quizz.js
+++ src/routes/quizz.js
@@ -6,6 +6,6 @@ const router = express.Router();
 router.post('/', createQuizz)
 router.get('/course/:courseId', getAllByCourseId)
 router.get('/:id', getById)
-router.get('/:id/cham-diem', markById)
+router.get('/cham-diem/:id', markById)
 
 export default router
