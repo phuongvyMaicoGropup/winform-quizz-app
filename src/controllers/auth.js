224e03303e8cc32c4e5282e4f1978bbc9dba773a
Phuong Vy <phuongvy.maicogroup@gmail.com>
2022-05-07 19:50:59 +0700

    updadte login api

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,44 +1,41 @@
-import Account from '../models/account'
+import Account from "../models/account";
 
 export const login = async (req, res) => {
-      const { userId, password, position } = req.body
-      try {
-            const user = await Account.findOne({ userId: userId })
-            if (!user)
-                  return res.status(404).json({ message: "user not found" })
-            if (user.password === password) {
-                  if (user.position === position)
-                        return res.status(200).json({ message: "login-success" })
-                  else return res.status(401).json({ message: "wrong-position" })
-            }
-            else
-                  return res.status(401).json({ message: "login-false" })
-      } catch (error) {
-            res.status(500).json({ error })
-      }
-}
+  const { userId, password, position } = req.body;
+  try {
+    const user = await Account.findOne({ userId: userId });
+    if (!user) return res.status(404).json({ message: "user not found" });
+    if (user.password === password) {
+      if (user.position === position)
+        return res.status(200).json({ message: "login-success", data: user });
+      else return res.status(401).json({ message: "wrong-position" });
+    } else return res.status(401).json({ message: "login-false" });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+};
 
 export const register = async (req, res) => {
-      const { userId, password, position } = req.body
-      try {
-            const newUser = new Account({
-                  userId,
-                  password,
-                  position
-            })
+  const { userId, password, position } = req.body;
+  try {
+    const newUser = new Account({
+      userId,
+      password,
+      position,
+    });
 
-            const user = await newUser.save()
-            res.status(200).json({ data: user })
-      } catch (error) {
-            console.log(error)
-            error.code === 11000
-            switch (error.code) {
-                  case 11000:
-                        res.status(409).json('duplicate')
-                        break
-                  default:
-                        res.status(500).json({ error })
-                        break
-            }
-      }
-}
+    const user = await newUser.save();
+    res.status(200).json({ data: user });
+  } catch (error) {
+    console.log(error);
+    error.code === 11000;
+    switch (error.code) {
+      case 11000:
+        res.status(409).json("duplicate");
+        break;
+      default:
+        res.status(500).json({ error });
+        break;
+    }
+  }
+};
